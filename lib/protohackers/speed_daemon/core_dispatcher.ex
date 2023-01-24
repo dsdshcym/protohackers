ec7d330024752534148de1fbdf7d20a75572f5e3
Yiming Chen <yiming@tubi.tv>
2023-01-24 23:12:05 +0800

    go through all tickets in CoreDispatcher
    & use disjoint? to decide if it's okay to issue this ticket

--- lib/protohackers/speed_daemon/core_dispatcher.ex
+++ lib/protohackers/speed_daemon/core_dispatcher.ex
@@ -15,23 +15,7 @@ defmodule Protohackers.SpeedDaemon.CoreDispatcher do
   end
 
   def handle_info(:run, state) do
-    case Protohackers.SpeedDaemon.Repository.query(state.repo, :undispatched_ticket) do
-      nil ->
-        :do_nothing
-
-      ticket ->
-        Registry.dispatch(state.registry, ticket.road, fn entries ->
-          if Enum.find(
-               entries,
-               fn {pid, true} ->
-                 match?(:ok, GenServer.call(pid, {:send_ticket, ticket}))
-               end
-             ),
-             do:
-               {:ok, _updated_repo} =
-                 Protohackers.SpeedDaemon.Repository.add(state.repo, :dispatched_ticket, ticket)
-        end)
-    end
+    dispatch_until_empty(state)
 
     schedule_run()
 
@@ -40,5 +24,54 @@ defmodule Protohackers.SpeedDaemon.CoreDispatcher do
 
   defp schedule_run() do
     Process.send_after(self(), :run, 100)
+  end
+
+  defp dispatch_until_empty(state) do
+    tickets = Protohackers.SpeedDaemon.Repository.query(state.repo, :tickets)
+
+    Enum.each(tickets, fn ticket ->
+      dispatched_tickets =
+        Protohackers.SpeedDaemon.Repository.query(state.repo, :dispatched_tickets)
+
+      if !same_car_same_day?(dispatched_tickets, ticket) do
+        Registry.dispatch(state.registry, ticket.road, fn entries ->
+          if Enum.find(
+               entries,
+               fn {pid, true} ->
+                 match?(:ok, GenServer.call(pid, {:send_ticket, ticket}))
+               end
+             ) do
+            {:ok, _updated_repo} =
+              Protohackers.SpeedDaemon.Repository.add(state.repo, :dispatched_ticket, ticket)
+          end
+        end)
+      end
+    end)
+  end
+
+  defp same_car_same_day?(tickets, ticket) do
+    ticket_set = to_day_set(ticket)
+
+    issued_set =
+      tickets
+      |> Enum.filter(&(&1.plate == ticket.plate))
+      |> Enum.map(&to_day_set/1)
+      |> Enum.reduce(MapSet.new(), &MapSet.union/2)
+
+    !MapSet.disjoint?(ticket_set, issued_set)
+  end
+
+  defp to_day_set(ticket) do
+    ticket
+    |> to_day_range()
+    |> MapSet.new()
+  end
+
+  defp to_day_range(ticket) do
+    to_day(ticket.from_timestamp)..to_day(ticket.to_timestamp)
+  end
+
+  defp to_day(timestamp) do
+    floor(timestamp / 86400)
   end
 end
